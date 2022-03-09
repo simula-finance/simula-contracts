731b70ce92f1e491877f01a9f21ad90a82c184d5
arch mage <softwarearchaeologist@gmail.com>
2022-03-09 18:13:49 +0530

    add skeletons

--- contracts/Simulacrum.sol
+++ contracts/Simulacrum.sol
@@ -5,11 +5,16 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+import "./interfaces/IERC20.sol";
+
 contract Simulacrum is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     constructor() initializer {}
 
-    address HYPR;
-    address simUSD;
+    IERC20 HYPR;
+    IERC20 simUSD;
+
+    mapping (address => uint) balances;
+    mapping (address => uint) debt;
 
     function initialize() public initializer {
         __Ownable_init();
@@ -23,12 +28,25 @@ contract Simulacrum is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     {}
 
     function setHypr(address _hypr) onlyOwner {
-        HYPR = _hypr;
+        HYPR = IERC20(_hypr);
     }
 
-    function setSimUSD() onlyOwner {
+
+    function setSimUSD(address _simUSD) onlyOwner {
+        simUSD = IERC20(_simUSD);
+    }
+
+    function _computeDebt(uint amount) internal returns (uint _debt) {
         
     }
 
-    function enter(uint amount) external {}
+    function enter(uint _amount) external {
+        require(HYPR.balanceOf(msg.sender) >= _amount, "SIMULA: Insufficient Collateral");
+        balances[msg.sender] += _amount;
+        
+    }
+
+    function exit() external {
+
+    }
 }
